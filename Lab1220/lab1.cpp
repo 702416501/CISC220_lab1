a0ec596998e5b18d607ea64f799e3558d96396c5
Allie <adeabreu@udel.edu>
2019-09-04 15:59:20 -0400

    commit changes through prob 8

--- Lab1220/lab1.cpp
+++ Lab1220/lab1.cpp
@@ -2,7 +2,7 @@
  * lab1.cpp
  *
  *  Created on: Aug 29, 2019
- *      Author: Allie DeAbreu,
+ *      Author: Allie DeAbreu and Alex Clark
  */
 
 #include <iostream>
@@ -50,11 +50,31 @@ int func5(){
 	return i;
 }//p5
 void func6(int x){
-	for (int i=1;i<x;i++){
-		if ()
+	for (int i=1;i<=12;i++){
+		cout<<i*x<<endl;
 	}
-}
+}//p6
 
+void func7(){
+	for (int i=1;i<=12;i++){
+		func6(i);
+	}
+}//p7
+
+int func8(){
+	int i, lcount=0;
+	cout<<"Please input a natural positive number"<<endl;
+	cin>>i;
+	while (i>1){
+		if (i%2==0){
+			i/=2;
+		} else{
+			i= (i*3)+1;
+		}
+		lcount++;
+	}
+	return lcount;
+}
 int main(){
 	cout<<"Hello, World!!"<<endl;//p1
 	func2();//p2
@@ -63,9 +83,13 @@ int main(){
 	func4();//p4
 	func2();//p2
 	cout<<func5()<<endl;//p5
-
-
-}//main
+	func2();//p2
+	func6(2);//p6; Need two more test cases
+	func2();//p2
+	func7();//p7
+	func2();//p2
+	cout<<func8()<<endl;
+}//main function
 
 
 
